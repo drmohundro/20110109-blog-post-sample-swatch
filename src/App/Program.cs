24e2db525963ad3963907d0eb4d28bb76039c5aa
David Mohundro <david@mohundro.com>
2016-12-27 20:21:11 -0600

    Add console app and begin better tests
    The tests are better because they are more granular... though I need
    more to cover all cases.

---
+++ src/App/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Swatch;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var accts = new List<Account>
+            {
+                new Account
+                {
+                    Name = "Amazon",
+                    IsPreferred = true,
+                    Moolah = 123456789.01m
+                },
+                new Account
+                {
+                    Name = "Bob's Store",
+                    IsPreferred = false,
+                    Moolah = 15.78m
+                }
+            };
+
+            var swatch = new Switcher<Account>();
+
+            var results = new List<string>();
+
+            foreach (var acct in accts)
+            {
+                swatch.Switch(acct,
+                    swatch.Case(x => x == null,
+                        x => Console.WriteLine("the object is null")),
+
+                    swatch.Case(x => x.Moolah > 10000.00m,
+                        x => Console.WriteLine("Whoa, these guys (" + x.Name + ") got some money")),
+
+                    swatch.Default(
+                        x => Console.WriteLine("wha?!?"))
+                );
+            }
+        }
+    }
+}
--- test/Swatch.Tests/SwitcherTests.cs
+++ test/Swatch.Tests/SwitcherTests.cs
@@ -1,50 +1,50 @@
-﻿using System.Collections.Generic;
-using Swatch;
+﻿using Swatch;
 using Xunit;
 
 namespace Tests
 {
-    public class Tests
+    public class SwitcherTests
     {
-        [Fact]
-        public void ShouldBeCorrect()
+        private readonly TestClass _instance;
+
+        public SwitcherTests()
         {
-            var accts = new List<Account>
-            {
-                new Account
-                {
-                    Name = "Amazon",
-                    IsPreferred = true,
-                    Moolah = 123456789.01m
-                },
-                new Account
-                {
-                    Name = "Bob's Store",
-                    IsPreferred = false,
-                    Moolah = 15.78m
-                }
-            };
-
-            var swatch = new Switcher<Account>();
-
-            var results = new List<string>();
-
-            foreach (var acct in accts)
-            {
-                swatch.Switch(acct,
-                    swatch.Case(x => x == null,
-                        x => results.Add("the object is null")),
-
-                    swatch.Case(x => x.Moolah > 10000.00m,
-                        x => results.Add("Whoa, these guys (" + x.Name + ") got some money")),
-
-                    swatch.Default(
-                        x => results.Add("default!"))
-                );
-            }
-
-            Assert.Equal(results[0], "Whoa, these guys (Amazon) got some money");
-            Assert.Equal(results[1], "default!");
+            _instance = new TestClass { Name = "Foo" };
         }
+
+        [Fact]
+        public void ShouldHandleTrueCases()
+        {
+            var swatch = new Switcher<TestClass>();
+
+            var result = false;
+
+            swatch.Switch(_instance,
+                swatch.Case(x => x.Name == "Foo",
+                    x => result = true)
+            );
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldSkipFalseCases()
+        {
+            var swatch = new Switcher<TestClass>();
+
+            var result = false;
+
+            swatch.Switch(_instance,
+                swatch.Case(x => x.Name == "Bar",
+                    x => result = true)
+            );
+
+            Assert.False(result);
+        }
+    }
+
+    class TestClass
+    {
+        public string Name { get; set; }
     }
 }
