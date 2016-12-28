e96e169df4d158340b206c1666b72080cae5744c
David Mohundro <david@mohundro.com>
2016-12-28 16:35:21 -0600

    Add a few more tests

--- test/Swatch.Tests/SwitcherTests.cs
+++ test/Swatch.Tests/SwitcherTests.cs
@@ -41,6 +41,65 @@ namespace Tests
 
             Assert.False(result);
         }
+
+        [Fact]
+        public void ShouldRunDefaultAlways()
+        {
+            var swatch = new Switcher<TestClass>();
+
+            var result = false;
+
+            swatch.Switch(_instance,
+                swatch.Default(
+                    x => result = true)
+            );
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldStopAfterFirstTrueCase()
+        {
+            var swatch = new Switcher<TestClass>();
+
+            int calls = 0;
+
+            swatch.Switch(_instance,
+                swatch.Case(
+                    x => {
+                        calls++;
+                        return false;
+                    },
+                    x => {
+                        // no op
+                    }
+                ),
+
+                swatch.Case(
+                    x => {
+                        calls++;
+                        return true;
+                    },
+                    x => {
+                        // no op
+                    }
+                ),
+
+                // stops here
+
+                swatch.Case(
+                    x => {
+                        calls++;
+                        return true;
+                    },
+                    x => {
+                        // no op
+                    }
+                )
+            );
+
+            Assert.Equal(calls, 2);
+        }
     }
 
     class TestClass
